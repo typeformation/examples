5e2b2d468a01ac8f52b559ffc2643c3fbc9fa100
Andrea Fiore <andrea.fiore@composable.io>
2017-10-30 23:24:01 +0000

    remove cerimony from httpBin code

--- httpbin/src/main/scala/httpbin/HttpBin.scala
+++ httpbin/src/main/scala/httpbin/HttpBin.scala
@@ -6,18 +6,16 @@ import org.http4s._
 import org.http4s.dsl._
 import com.redis._
 import org.http4s.headers.`Content-Type`
-import fs2.Stream
 
 class HttpBin(redisClient: RedisClient) {
   val service = HttpService {
     case req @ POST -> Root / "bin" =>
       val uid = UUID.randomUUID()
-
-      req.bodyAsText.flatMap { body =>
+      req.as[String].flatMap { body =>
         redisClient.set(s"bin/$uid", body)
         req.contentType.foreach(ct => redisClient.set(s"bin/$uid/content-type", ct.toRaw.value))
-        Stream.eval(Created(s"$uid\n"))
-      }.runFold[Response](Response(status = InternalServerError))((_, resp) => resp)
+        Created(s"$uid\n")
+      }
 
     case GET -> Root / "bin" / uid =>
       val entity = for {
